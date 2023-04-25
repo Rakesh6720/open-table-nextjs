d24b5add535341e590279ac10a5a96c934299600
Rakesh Baruah <rbaruah@gmail.com>
2023-04-25 10:48:27 -0500

    add star ratings and db reviews ratings and users

--- app/restaurant/[slug]/components/ReviewCard.tsx
+++ app/restaurant/[slug]/components/ReviewCard.tsx
@@ -1,5 +1,6 @@
 import { Review } from "@prisma/client";
 import React from "react";
+import Stars from "../../../components/Stars";
 
 const ReviewCard = ({ review }: { review: Review }) => {
   return (
@@ -8,7 +9,8 @@ const ReviewCard = ({ review }: { review: Review }) => {
         <div className="w-1/6 flex flex-col items-center">
           <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
             <h2 className="text-white text-2xl uppercase">
-              {review.first_name[0]} {review.last_name[0]}
+              {review.first_name[0]}
+              {review.last_name[0]}
             </h2>
           </div>
           <p className="text-center">
@@ -17,7 +19,9 @@ const ReviewCard = ({ review }: { review: Review }) => {
         </div>
         <div className="ml-10 w-5/6">
           <div className="flex items-center">
-            <div className="flex mr-5">*****</div>
+            <div className="flex mr-5">
+              <Stars rating={review.rating} reviews={[]} />
+            </div>
           </div>
           <div className="mt-5">
             <p className="text-lg font-light">{review.text}</p>
