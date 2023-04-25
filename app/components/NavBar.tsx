4753ae1928ec5e6f3bf72850fde4cf6dc576e84a
Rakesh Baruah <rbaruah@gmail.com>
2023-04-25 12:05:55 -0500

    setup material ui modal with sign in and sign up

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,18 +1,16 @@
 import Link from "next/link";
+import LoginModal from "./LoginModal";
 
 export default function NavBar() {
   return (
     <nav className="bg-white p-2 flex justify-between">
       <Link href="" className="font-bold text-gray-700 text-2x">
-        {" "}
-        Open Table{" "}
+        Open Table
       </Link>
       <div>
         <div className="flex">
-          <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-            Sign in
-          </button>
-          <button className="border p-1 px-4 rounded">Sign up</button>
+          <LoginModal isSignIn={true} />
+          <LoginModal isSignIn={false} />
         </div>
       </div>
     </nav>
