af3058d6d95abae8be8c5bda61a1967379f4f7ec
Rakesh Baruah <rbaruah@gmail.com>
2023-04-25 15:41:28 -0500

    parse jwt from response

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import * as jose from "jose";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,17 +12,13 @@ export default async function handler(
   const bearerToken = req.headers["authorization"] as string;
 
   if (!bearerToken) {
-    return res
-      .status(401)
-      .json({ errorMessage: "Unauthorized request (no bearer token)" });
+    return res.status(401).json({ errorMessage: "Unauthorized request" });
   }
 
   const token = bearerToken.split(" ")[1];
 
   if (!token) {
-    return res
-      .status(401)
-      .json({ errorMessage: "Unauthorized request (no token)" });
+    return res.status(401).json({ errorMessage: "Unauthorized request" });
   }
 
   const secret = new TextEncoder().encode(process.env.JWT_SECRET);
@@ -26,10 +26,28 @@ export default async function handler(
   try {
     await jose.jwtVerify(token, secret);
   } catch (error) {
-    return res
-      .status(401)
-      .json({ errorMessage: "Unauthorized request (token invalid)" });
+    return res.status(401).json({ errorMessage: "Unauthorized request" });
   }
 
-  return res.json({ me: "Rakesh" });
+  const payload = jwt.decode(token) as { email: string };
+
+  if (!payload.email) {
+    return res.status(401).json({ errorMessage: "Unauthorized request" });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: payload.email,
+    },
+    select: {
+      id: true,
+      first_name: true,
+      last_name: true,
+      email: true,
+      city: true,
+      phone: true,
+    },
+  });
+
+  return res.json({ me: user });
 }
